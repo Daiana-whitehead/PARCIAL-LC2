69ee6c26e92b57044d950e85633c81c6661cafb3
Daiana-whitehead <95387764+Daiana-whitehead@users.noreply.github.com>
2022-06-30 19:10:00 -0300

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,42 @@
+const Btn = document.getElementById('btn');
+var capacidad = document.querySelector('capacidad');
+var ResultMessages = document.getElementById('resultMesaje');
+const selectColor = document.getElementById('color');
+var imageIphone = document.getElementById('img-color');
+var stringvalidations = document.getElementById('error');
+
+/*CAMBIO DE IMAGEN*/
+selectColor.onchange = function(select) {
+    imageIphone.src = "IMG/iphone13/" + select.target.value + ".png";
+}
+
+/*CALCULAR PRECIO*/
+Btn.onclick = function calculatePrice() {
+    var inputNum = document.getElementById("number");
+    var cant = inputNum.value;
+
+    /*VALIDACIONES*/
+    if (selectColor == 0 || capacidad == 0 || cant <= 0 || cant > 10) {
+        stringvalidations.innerHTML += '<h2>Error: ingresar valores validos</h2>';
+        stringvalidations.style.display = "block";
+        return;
+    }
+
+    switch (capacidad) {
+        case "128":
+            precio == 150.000 * cant;
+            ResultMessages.innerHTML += ' <h3>Cotizacion: $ ', { precio }, '</h3>';
+            ResultMessages.style.display = "block";
+            break;
+        case "256":
+            precio == 165.000 * cant;
+            ResultMessages.innerHTML += ' <h3>Cotizacion: $', { precio }, '</h3>';
+            ResultMessages.style.display = "block";
+            break;
+        case "512":
+            precio == 180.000 * cant;
+            ResultMessages.innerHTML += ' <h3>Cotizacion: $', { precio }, '</h3>';
+            ResultMessages.style.display = "block";
+            break;
+    }
+}
